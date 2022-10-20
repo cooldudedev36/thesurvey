3a32fe433f51676d557074c248a3bdcefcbb7beb
Cooldudedev32 <cooldudedev36@proton.me>
2022-10-20 11:38:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import time
+import tk
+import pip
+import turtle
+
+print("made in python")
+time.sleep(2)
+print("booting surveybot 0.1 ...........")
+time.sleep(2)
+print("booted survey.")
+print("<unknown>welcome to surveybot 0.1")
+time.sleep(2)
+print("")
+print("<unknown>you can call me s.a.i.")
+time.sleep(2)
+print("")
+print("<s.a.i>it stands for survey artfical intellgence")
+time.sleep(2)
+print("")
+print("<s.a.i>you are participating in the s.a.i experiment to test the capabilities of a robot")
+time.sleep(2)
+print("")
+print("<s.a.i>you will be doing a series of simple questions")
+time.sleep(2)
+print("")
+print("<s.a.i>what is your favorite color?")
+q1=(input())
+print("<you>",(q1))
+time.sleep(2)
+print("") 
+print("<s.a.i>i have to go")
+time.sleep(2)
+print("")
+print("<system>s.a.i disconnected")
+time.sleep(2)
+print("")
+print("<system>caller connected")
+time.sleep(2)
+print("")
+print("<unknown>hello are you there?")
+time.sleep(2)
+print("")
+print("<unknown>i am calling from customer service do not finish the survey what ever you do not finish this sur............")
+time.sleep(2)
+print("")
+print("<system>caller disconnected")
+time.sleep(2)
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
